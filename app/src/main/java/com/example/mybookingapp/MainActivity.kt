353e605a2343685bf6c2111f4b3f458d4ce0e546
DrewP-Dev <andrpilgame@gmail.com>
2019-07-09 12:06:38 +0300

    Initial Commit

--- app/src/main/java/com/example/mybookingapp/MainActivity.kt
+++ app/src/main/java/com/example/mybookingapp/MainActivity.kt
@@ -1,25 +1,54 @@
 package com.example.mybookingapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.mybookingapp.R.id.*
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_drawer.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(findViewById(R.id.main_toolbar))
+        setSupportActionBar(toolbar)
+        init()
 
         val actionBar = supportActionBar
 
         actionBar!!.title = "Cicket Booking"
 
-        // Display the app icon in toolbar
-        actionBar.setDisplayShowHomeEnabled(true)
-        actionBar.setLogo(R.mipmap.ic_launcher_app)
-        actionBar.setDisplayUseLogoEnabled(true)
+    }
+
+    private fun init() {
+        val toggle = ActionBarDrawerToggle(Activity(), drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        navigation_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            booking_btn->{
+
+            }
+            about_btn ->{
+
+            }
+            settings_btn ->{
+
+            }
+            else ->{
+
+            }
+        }
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
